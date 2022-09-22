685667d353c029f8bb00f0ceefc908cb045d1943
AWeidenkopf <oliveira.anacalado@gmail.com>
2022-09-22 17:22:05 -0400

    addd second-request functionality

--- controllers/skills.js
+++ controllers/skills.js
@@ -18,8 +18,33 @@ function index(req, res) {
     res.render('skills/new')
   }
 
+  function create(req,res) {
+    Skill.create(req.body)
+    .then(skill => {
+      res.redirect('/skills')
+    })
+    .catch(error => {
+      console.log(error)
+      res.redirect('/')
+    })
+  }
+
+  function show(req, res) {
+    Skill.findById(req.params.id)
+    .then(skill => {
+      res.render('skills/show', {
+        skill: skill
+      })
+    })
+    .catch(error => {
+      console.log(error)
+      res.redirect('/')
+    })
+  }
 
 export {
   index,
-  newSkill as new
+  newSkill as new,
+  create,
+  show
 }
